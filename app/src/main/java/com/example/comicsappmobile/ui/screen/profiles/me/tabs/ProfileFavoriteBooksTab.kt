b98381b5b2d614699559e13d5cf6a6791328ce9c
Veji <stretchable.ru@gmail.com>
2025-02-02 21:20:42 +0300

    Added visible mode to user added books in Profile Screen. Added information message for user, where his list of favorite, my comments or added titles is empty

--- app/src/main/java/com/example/comicsappmobile/ui/screen/profiles/me/tabs/ProfileFavoriteBooksTab.kt
+++ app/src/main/java/com/example/comicsappmobile/ui/screen/profiles/me/tabs/ProfileFavoriteBooksTab.kt
@@ -2,13 +2,19 @@ package com.example.comicsappmobile.ui.screen.profiles.me.tabs
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.comicsappmobile.ui.components.ThemedErrorCard
@@ -45,9 +51,26 @@ fun ProfileFavoriteBooksTab(
             is UiState.Loading -> { CircularProgressIndicator() }
             is UiState.Success -> {
                 val stars: List<Pair<BookUiModel, UserFavoriteUiModel>> = if (userStarsBooks.data != null) userStarsBooks.data!! else emptyList()
+                if (stars.isEmpty()){
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ){
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "В избранном пусто",
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                fontWeight = FontWeight.Bold,
+                                color = MaterialTheme.colorScheme.primary,
+                                textAlign = TextAlign.Center
+                            ),
+                        )
+                    }
+                }else{
                     for((book, favorite) in stars){
                         ProfileFavoriteBookCard(book, favorite, navController)
                     }
+                }
             }
         }
     }
